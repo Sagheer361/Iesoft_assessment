bdec69cff16e489cfbea2c47fec28eedca3e6e7a
Sagheer Ahamed <sagh@SAGHEER123.com>
2024-07-06 11:02:08 +0530

    updated

---
+++ src/main.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Login from './Pages/Login1.tsx';
+import DataDisplay from './Pages/DataDisplay.tsx';
+import Signup from './Pages/SignUp1.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Signup />,
+  },
+   {
+    path: "/login",
+    element: <Login />,
+  },
+   {
+    path: "/data",
+    element: <DataDisplay />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+    </React.StrictMode>,
+)
